2352d0c030c5d87e9a0c964f977b7e9911b90cd7
szewczyk.franciszek02 <szewczyk.franciszek02@gmail.com>
2023-01-28 19:54:20 +0100

    Progress indicator

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,11 @@ int main(int argc, char *argv[]) {
     std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
 
     for (int y = imageHeight - 1; y >= 0; --y) {
+        std::cerr << "\rScanlines remaining: " << y << ' ' << std::flush;
         for (int x = 0; x < imageWidth; ++x) {
-            auto r = double(x) / (imageWidth - 1);
+            auto r = double(x / 2) / (imageWidth - 1);
             auto g = double(y) / (imageHeight - 1);
-            auto b = 0.25;
+            auto b = 0.55;
 
             int ir = static_cast<int>(255.999 * r);
             int ig = static_cast<int>(255.999 * g);
@@ -19,4 +20,8 @@ int main(int argc, char *argv[]) {
             std::cout << ir << ' ' << ig << ' ' << ib << "\n";
         }
     }
+
+    std::cerr << "\nDone\n";
+
+    return 0;
 }
