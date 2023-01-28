377155b68d018bedbac06a52ecea7e43b59c5fca
szewczyk.franciszek02 <szewczyk.franciszek02@gmail.com>
2023-01-28 19:51:22 +0100

    Basic gradient

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Ray Tracing
+
+## Building
+
+### Setup
+
+Everything should be taken care of if you just do this.
+
+```sh
+chmod +x ./build.sh
+chmod +x ./run.sh
+
+./build.sh
+./run.sh
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,22 @@
-#include <stdlib.h>
+#include <iostream>
 
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) {
+    const int imageWidth = 256;
+    const int imageHeight = 256;
+
+    std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+
+    for (int y = imageHeight - 1; y >= 0; --y) {
+        for (int x = 0; x < imageWidth; ++x) {
+            auto r = double(x) / (imageWidth - 1);
+            auto g = double(y) / (imageHeight - 1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << "\n";
+        }
+    }
+}
