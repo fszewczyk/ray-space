dd880af9117c24de1ab6b21dbe3c94c26d1f02f7
fszewczyk <szewczyk.franciszek02@gmail.com>
2023-03-08 19:23:58 +0100

    Build and run scrips minor changes

--- build.sh
+++ build.sh
@@ -1,3 +1,28 @@
+#!/bin/bash
+set -e 
+
 cd build
 cmake ..
 make
+cd ..
+
+RUN="false"
+
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -r|--run)
+      RUN="true"
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+if [ "$RUN" == "true" ]; then
+  source ./run.sh
+fi
