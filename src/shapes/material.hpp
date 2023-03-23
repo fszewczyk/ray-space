78dc6d646771205ca342c003ad8ed4b306a0aeac
szewczyk.franciszek02 <szewczyk.franciszek02@gmail.com>
2023-03-23 10:58:49 +0100

    Small changes

--- src/shapes/material.hpp
+++ src/shapes/material.hpp
@@ -14,70 +14,80 @@ typedef struct hitData hitData;
 enum MATERIAL_TYPE { LAMBERTIAN, METAL, REFRACTOR, DIFFUSE_LIGHT };
 
 class material {
-  public:
-    virtual bool scatter(const ray &rayIn, const hitData &data, color &attenuation, ray &rayOut) const = 0;
-    virtual color emit(double u, double v, const point3 &p, bool firstHit) const;
-    virtual std::shared_ptr<solidColor> getLightMaterial() const;
-    virtual color getVisibleColor() const;
+public:
+  virtual bool scatter(const ray &rayIn, const hitData &data,
+                       color &attenuation, ray &rayOut) const = 0;
+  virtual color emit(double u, double v, const point3 &p, bool firstHit) const;
+  virtual std::shared_ptr<solidColor> getLightMaterial() const;
+  virtual color getVisibleColor() const;
 };
 
 class lambertian : public material {
-  public:
-    lambertian(const color &c);
-    lambertian(shared_ptr<texture> c);
+public:
+  lambertian(const color &c);
+  lambertian(shared_ptr<texture> c);
 
-    static std::shared_ptr<lambertian> generateFromImage(std::shared_ptr<image> im);
-    static std::shared_ptr<lambertian> generateFromImageTextureType(int imageTextureType);
+  static std::shared_ptr<lambertian>
+  generateFromImage(std::shared_ptr<image> im);
+  static std::shared_ptr<lambertian>
+  generateFromImageTextureType(int imageTextureType);
 
-    virtual bool scatter(const ray &rayIn, const hitData &data, color &attenuation, ray &rayOut) const override;
-    virtual color getVisibleColor() const;
+  virtual bool scatter(const ray &rayIn, const hitData &data,
+                       color &attenuation, ray &rayOut) const override;
+  virtual color getVisibleColor() const override;
 
-  private:
-    shared_ptr<texture> m_albedo;
+private:
+  shared_ptr<texture> m_albedo;
 };
 
 class metal : public material {
-  public:
-    metal(const color &c, double f);
+public:
+  metal(const color &c, double f);
 
-    virtual bool scatter(const ray &rayIn, const hitData &data, color &attenuation, ray &rayOut) const override;
-    virtual color getVisibleColor() const;
+  virtual bool scatter(const ray &rayIn, const hitData &data,
+                       color &attenuation, ray &rayOut) const override;
+  virtual color getVisibleColor() const override;
 
-  private:
-    color m_albedo;
-    double m_fuzz;
+private:
+  color m_albedo;
+  double m_fuzz;
 };
 
 class refractor : public material {
-  public:
-    refractor(double eta);
+public:
+  refractor(double eta);
 
-    virtual bool scatter(const ray &rayIn, const hitData &data, color &attenuation, ray &rayOut) const override;
+  virtual bool scatter(const ray &rayIn, const hitData &data,
+                       color &attenuation, ray &rayOut) const override;
 
-  private:
-    static double reflectance(double cosine, double eta);
+private:
+  static double reflectance(double cosine, double eta);
 
-    double m_eta;
+  double m_eta;
 };
 
 class diffuseLight : public material {
-  public:
-    diffuseLight(shared_ptr<texture> text, color lightColor);
-    diffuseLight(color even, color odd, color lightColor);
-    diffuseLight(color displayColor, color lightColor);
+public:
+  diffuseLight(shared_ptr<texture> text, color lightColor);
+  diffuseLight(color even, color odd, color lightColor);
+  diffuseLight(color displayColor, color lightColor);
 
-    static std::shared_ptr<diffuseLight> generateFromImage(std::shared_ptr<image> im, color c);
-    static std::shared_ptr<diffuseLight> generateFromImageTextureType(IMAGE_TEXTURE_TYPE imageTextureType, color c);
+  static std::shared_ptr<diffuseLight>
+  generateFromImage(std::shared_ptr<image> im, color c);
+  static std::shared_ptr<diffuseLight>
+  generateFromImageTextureType(IMAGE_TEXTURE_TYPE imageTextureType, color c);
 
-    virtual bool scatter(const ray &rayIn, const hitData &data, color &attenuation, ray &rayOut) const override;
-    virtual color emit(double u, double v, const point3 &p, bool firstHit) const override;
+  virtual bool scatter(const ray &rayIn, const hitData &data,
+                       color &attenuation, ray &rayOut) const override;
+  virtual color emit(double u, double v, const point3 &p,
+                     bool firstHit) const override;
 
-    virtual std::shared_ptr<solidColor> getLightMaterial() const override;
-    virtual color getVisibleColor() const;
+  virtual std::shared_ptr<solidColor> getLightMaterial() const override;
+  virtual color getVisibleColor() const override;
 
-  private:
-    shared_ptr<texture> m_textureToDisplay;
-    shared_ptr<solidColor> m_lightColor;
+private:
+  shared_ptr<texture> m_textureToDisplay;
+  shared_ptr<solidColor> m_lightColor;
 };
 
 } // namespace shkyera
--- src/ui/settings/exportSettingsWindow.cpp
+++ src/ui/settings/exportSettingsWindow.cpp
@@ -1,3 +1,4 @@
+#include <filesystem>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -9,133 +10,150 @@
 
 namespace shkyera {
 
-exportSettingsWindow::exportSettingsWindow(std::shared_ptr<camera> cam) : m_camera(cam) {}
+exportSettingsWindow::exportSettingsWindow(std::shared_ptr<camera> cam)
+    : m_camera(cam) {}
 
 exportSettings exportSettingsWindow::render(RENDER_MODE &mode) {
-    ImGui::Begin("Export");
+  ImGui::Begin("Export");
 
-    static exportSettings settings = getDefaultExportSettings();
-    int setWidth = settings.width;
-    int setHeight = settings.height;
+  static exportSettings settings = getDefaultExportSettings();
+  int setWidth = settings.width;
+  int setHeight = settings.height;
 
-    float aspectRatio = static_cast<float>(settings.width) / settings.height;
+  float aspectRatio = static_cast<float>(settings.width) / settings.height;
 
-    if (ImGui::BeginCombo("Format", (" " + FILE_EXTENSIONS[settings.extension]).c_str())) {
-        if (ImGui::Selectable("PNG", settings.extension == PNG))
-            settings.extension = PNG;
-        if (ImGui::Selectable("JPG", settings.extension == JPG))
-            settings.extension = JPG;
-        ImGui::EndCombo();
-    }
+  if (ImGui::BeginCombo("Format",
+                        (" " + FILE_EXTENSIONS[settings.extension]).c_str())) {
+    if (ImGui::Selectable("PNG", settings.extension == PNG))
+      settings.extension = PNG;
+    if (ImGui::Selectable("JPG", settings.extension == JPG))
+      settings.extension = JPG;
+    ImGui::EndCombo();
+  }
 
-    static char fileName[128] = "shkyera_render";
-    ImGui::InputText("File Name", fileName, IM_ARRAYSIZE(fileName));
+  static char fileName[128] = "shkyera_render";
+  ImGui::InputText("File Name", fileName, IM_ARRAYSIZE(fileName));
 
-    static const char *localDirectory = settings.path.c_str();
-    static char directory[1024];
-    static bool loadedLocalDirectory = false;
-    if (!loadedLocalDirectory) {
-        strcpy(directory, localDirectory);
-        loadedLocalDirectory = true;
-    }
+  static const char *localDirectory = settings.path.c_str();
+  static char directory[1024];
+  static bool loadedLocalDirectory = false;
+  if (!loadedLocalDirectory) {
+    strcpy(directory, localDirectory);
+    loadedLocalDirectory = true;
+  }
 
-    ImGui::TextWrapped(directory);
-    if (ImGui::Button("Choose Destination")) {
-        FILE *f = popen("zenity  --file-selection --directory --title=\"Choose a directory\"", "r");
-        fgets(directory, 1024, f);
-    }
+  ImGui::TextWrapped(directory);
 
-    settings.path = directory;
-    settings.path.pop_back();
-    settings.path += "/";
-    settings.path += fileName;
+#ifndef __APPLE__
+  if (ImGui::Button("Choose Destination")) {
+    FILE *f = popen(
+        "zenity  --file-selection --directory --title=\"Choose a directory\"",
+        "r");
+    fgets(directory, 1024, f);
+  }
+#endif
 
-    switch (settings.extension) {
-    case PNG:
-        settings.path += ".png";
-        break;
-    case JPG:
-    default:
-        settings.path += ".jpg";
-        break;
-    }
+  settings.path = directory;
+#ifndef __APPLE__
+  settings.path.pop_back();
+#endif
+  settings.path += "/";
+  settings.path += fileName;
 
-    ImGui::Checkbox("Lock Aspect Ratio", &settings.lockAspectRatio);
+  switch (settings.extension) {
+  case PNG:
+    settings.path += ".png";
+    break;
+  case JPG:
+  default:
+    settings.path += ".jpg";
+    break;
+  }
 
-    ImGui::SliderInt("Width", &setWidth, 128, 4096);
-    ImGui::SliderInt("Height", &setHeight, 128, 4096);
+  ImGui::Checkbox("Lock Aspect Ratio", &settings.lockAspectRatio);
 
-    if (settings.lockAspectRatio) {
-        if (setWidth != settings.width)
-            setHeight = setWidth / aspectRatio;
-        else if (setHeight != settings.height)
-            setWidth = setHeight * aspectRatio;
-    }
+  ImGui::SliderInt("Width", &setWidth, 128, 4096);
+  ImGui::SliderInt("Height", &setHeight, 128, 4096);
 
-    settings.width = std::max(128, setWidth);
-    settings.height = std::max(128, setHeight);
+  if (settings.lockAspectRatio) {
+    if (setWidth != settings.width)
+      setHeight = setWidth / aspectRatio;
+    else if (setHeight != settings.height)
+      setWidth = setHeight * aspectRatio;
+  }
 
-    ImGui::SliderInt("Ray Depth", &settings.maximumRayDepth, 2, 10);
-    if (ImGui::IsItemHovered() && !ImGui::IsItemActive()) {
-        ImGui::BeginTooltip();
-        ImGui::TextUnformatted(
-            "Maximum number of bounces of every ray.\nUse large values only if your objects are very "
-            "close to each other.");
-        ImGui::EndTooltip();
-    }
+  settings.width = std::max(128, setWidth);
+  settings.height = std::max(128, setHeight);
 
-    ImGui::SliderInt("Rays Per Pixel", &settings.raysPerPixel, 10, 200);
-    if (ImGui::IsItemHovered() && !ImGui::IsItemActive()) {
-        ImGui::BeginTooltip();
-        ImGui::TextUnformatted("Rays shot out for every pixel.\nThe higher, the less noisy the image.\nUse high values "
-                               "if you have relatively small light sources.");
-        ImGui::EndTooltip();
-    }
+  ImGui::SliderInt("Ray Depth", &settings.maximumRayDepth, 2, 10);
+  if (ImGui::IsItemHovered() && !ImGui::IsItemActive()) {
+    ImGui::BeginTooltip();
+    ImGui::TextUnformatted("Maximum number of bounces of every ray.\nUse large "
+                           "values only if your objects are very "
+                           "close to each other.");
+    ImGui::EndTooltip();
+  }
 
-    ImGui::Dummy(ImVec2(0, 5.0f));
+  ImGui::SliderInt("Rays Per Pixel", &settings.raysPerPixel, 10, 200);
+  if (ImGui::IsItemHovered() && !ImGui::IsItemActive()) {
+    ImGui::BeginTooltip();
+    ImGui::TextUnformatted("Rays shot out for every pixel.\nThe higher, the "
+                           "less noisy the image.\nUse high values "
+                           "if you have relatively small light sources.");
+    ImGui::EndTooltip();
+  }
 
-    if (mode == EDIT) {
-        if (ImGui::Button("Start Preview", ImVec2(160, 0))) {
-            mode = PREVIEW;
-        }
-        if (ImGui::IsItemHovered() && !ImGui::IsItemActive()) {
-            ImGui::BeginTooltip();
-            ImGui::TextUnformatted("The program might run slower after running preview.");
-            ImGui::EndTooltip();
-        }
-    } else {
-        if (ImGui::Button("Stop Preview", ImVec2(160, 0))) {
-            mode = EDIT;
-        }
-    }
+  ImGui::Dummy(ImVec2(0, 5.0f));
 
-    ImGui::SameLine();
-
-    if (ImGui::Button("Export", ImVec2(160, 0))) {
-        mode = EXPORT;
+  if (mode == EDIT) {
+    if (ImGui::Button("Start Preview", ImVec2(160, 0))) {
+      mode = PREVIEW;
     }
     if (ImGui::IsItemHovered() && !ImGui::IsItemActive()) {
-        ImGui::BeginTooltip();
-        ImGui::TextUnformatted("The program might run slower after starting the export.");
-        ImGui::EndTooltip();
+      ImGui::BeginTooltip();
+      ImGui::TextUnformatted(
+          "The program might run slower after running preview.");
+      ImGui::EndTooltip();
     }
+  } else {
+    if (ImGui::Button("Stop Preview", ImVec2(160, 0))) {
+      mode = EDIT;
+    }
+  }
 
-    ImGui::End();
+  ImGui::SameLine();
 
-    return settings;
+  if (ImGui::Button("Export", ImVec2(160, 0))) {
+    mode = EXPORT;
+  }
+  if (ImGui::IsItemHovered() && !ImGui::IsItemActive()) {
+    ImGui::BeginTooltip();
+    ImGui::TextUnformatted(
+        "The program might run slower after starting the export.");
+    ImGui::EndTooltip();
+  }
+
+  ImGui::End();
+
+  return settings;
 } // namespace shkyera
 
 exportSettings exportSettingsWindow::getDefaultExportSettings() {
-    exportSettings settings;
-    settings.width = 1920;
-    settings.height = 1080;
-    settings.maximumRayDepth = 5;
-    settings.raysPerPixel = 40;
-    settings.path = get_current_dir_name();
-    settings.extension = PNG;
-    settings.lockAspectRatio = true;
+  exportSettings settings;
+  settings.width = 1920;
+  settings.height = 1080;
+  settings.maximumRayDepth = 5;
+  settings.raysPerPixel = 40;
+  settings.extension = PNG;
+  settings.lockAspectRatio = true;
 
-    return settings;
+#ifdef __APPLE__
+  settings.path = std::filesystem::current_path();
+#else
+  settings.path = get_current_dir_name();
+#endif
+
+  return settings;
 }
 
 } // namespace shkyera
